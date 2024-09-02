fc937cc15b4433d4a9835bf8bdc694ff1986161e
itsAnanth <danonymousarmy@gmai.com>
2024-09-02 23:23:00 +0530

    feat: create protected routes

---
+++ src/components/Protected/Protected.tsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { FireBaseContext } from "../../contexts/firebase.context";
+import { Navigate } from "react-router-dom";
+
+export default function Protected({ children }: any) {
+
+    const authContext = useContext(FireBaseContext);
+    const user = authContext.user
+
+    return (
+        <>
+            {user ? (children) : (<Navigate to={'/login'} />)}
+        </>
+    )
+}
