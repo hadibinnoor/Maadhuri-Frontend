e20a7297b1fcffdeea8f85ba2219ac58e81df471
Ananth <64283902+itsAnanth@users.noreply.github.com>
2024-09-02 23:32:17 +0530

    Update database.ts

--- src/structures/database.ts
+++ src/structures/database.ts
@@ -8,14 +8,7 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBHT9wDkmxWs7T6a6m7ZNvFOY6JKJaE5Og",
-    authDomain: "blitzappv1.firebaseapp.com",
-    projectId: "blitzappv1",
-    storageBucket: "blitzappv1.appspot.com",
-    databaseURL: "https://blitzappv1-default-rtdb.asia-southeast1.firebasedatabase.app",
-    messagingSenderId: "245689724814",
-    appId: "1:245689724814:web:cb42ab432716d16de87edf",
-    measurementId: "G-197ZK5TQZK"
+
 };
 
 // Initialize Firebase
